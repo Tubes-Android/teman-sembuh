a958509a4aa9fef761c0fd633de828ef0f5b738b
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2022-01-24 19:46:00 +0700

    feat: add navbar menu

---
+++ app/src/main/java/com/example/temansembuh/BottomNavGraph.kt
@@ -0,0 +1,29 @@
+package com.example.temansembuh
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.temansembuh.screens.BedScreen
+import com.example.temansembuh.screens.HomeScreen
+import com.example.temansembuh.screens.HospitalScreen
+import com.example.temansembuh.screens.ProfileScreen
+
+@Composable
+fun BottomNavGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = BottomBarScreen.Home.route) {
+        composable(route = BottomBarScreen.Home.route) {
+            HomeScreen()
+        }
+        composable(route = BottomBarScreen.Hospital.route) {
+            HospitalScreen()
+        }
+        composable(route = BottomBarScreen.Bed.route) {
+            BedScreen()
+        }
+        composable(route = BottomBarScreen.Profile.route) {
+            ProfileScreen()
+        }
+    }
+}
